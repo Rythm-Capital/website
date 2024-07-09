93001e774b90f79df6cefc0e6034442f9bdaef4a
raymond <raymond.luke.spartz@gmail.com>
2024-07-09 12:45:49 -0500

    tailwindmjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,8 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
-};
+}
