77371bb139cfdd74a233a326f5fa98a62ec8cdb2
raymond <raymond.luke.spartz@gmail.com>
2024-07-08 19:19:57 -0500

    css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,33 @@
 import { loadEnv } from "vite";
 const {
-	PUBLIC_SANITY_STUDIO_PROJECT_ID,
-	PUBLIC_SANITY_STUDIO_DATASET,
-	PUBLIC_SANITY_PROJECT_ID,
-	PUBLIC_SANITY_DATASET,
+  PUBLIC_SANITY_STUDIO_PROJECT_ID,
+  PUBLIC_SANITY_STUDIO_DATASET,
+  PUBLIC_SANITY_PROJECT_ID,
+  PUBLIC_SANITY_DATASET
 } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import sanity from "@sanity/astro";
 import { defineConfig } from "astro/config";
-
 const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID || PUBLIC_SANITY_PROJECT_ID;
 const dataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
 
+// https://astro.build/config
 export default defineConfig({
-	output: "hybrid",
-	adapter: vercel(),
-	integrations: [
-		sanity({
-			projectId,
-			dataset,
-			studioBasePath: "/studio",
-			useCdn: false,
-			apiVersion: "2023-03-20",
-		}),
-		react(),
-		tailwind(),
-	],
-	redirects: {
-		"/studio/[...slug]": "/studio",
-	},
-	server: { port: 3000 },
+  output: "hybrid",
+  adapter: vercel(),
+  integrations: [sanity({
+    projectId,
+    dataset,
+    studioBasePath: "/studio",
+    useCdn: false,
+    apiVersion: "2023-03-20"
+  }), react(), tailwind()],
+  redirects: {
+    "/studio/[...slug]": "/studio"
+  },
+  server: {
+    port: 3000
+  }
 });
